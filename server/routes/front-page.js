4c2b9a88fba414eba2ec2fb2736da003394c3c8b
Viktor Charypar <charypar@gmail.com>
2015-06-15 17:24:35 +0100

    Basic fastFT fetching

--- server/routes/front-page.js
+++ server/routes/front-page.js
@@ -3,29 +3,45 @@
 var ContentApi = require('../services/content-api');
 
 var uuids = {
-	ukTop: '520ddb76-e43d-11e4-9e89-00144feab7de',
-	intlTop: 'b0d8e4fe-10ff-11e5-8413-00144feabdc0'
+	ukTop: '520ddb76-e43d-11e4-9e89-00144feab7de', // list id
+	intlTop: 'b0d8e4fe-10ff-11e5-8413-00144feabdc0', // list id
+	fastFt: '5c7592a8-1f0c-11e4-b0cb-b2227cce2b54', // concept id
 }
 
+// Content cache for polling
 var content = {
-	elastic: [],
-	capi1: []
+	elastic: {
+		articles: [],
+		fastFt: []
+	},
+	capi1: {
+		articles: [],
+		fastFt: []
+	}
 }
 
 // Poll both APIs so that we can feature flag between them
 
 ContentApi.pollList(uuids.ukTop, true, function(page) {
-	content.elastic = page;
+	content.elastic.articles = page;
 });
 
 ContentApi.pollList(uuids.ukTop, false, function(page) {
-	content.capi1 = page;
+	content.capi1.articles = page;
+});
+
+ContentApi.pollFastFt(uuids.fastFt, true, function(fastFt) {
+	content.elastic.fastFt = fastFt;
+});
+
+ContentApi.pollFastFt(uuids.fastFt, false, function(fastFt) {
+	content.capi1.fastFt = fastFt;
 });
 
 module.exports = function(req, res, next) {
-	var articles = (res.locals.flags.elasticSearchItemGet.isSwitchedOn ? content.elastic : content.capi1);
-	if(articles.items && articles.items.slice) {
-		articles.items = articles.items.slice(0, 10);
+	var source = (res.locals.flags.elasticSearchItemGet.isSwitchedOn ? content.elastic : content.capi1);
+	if(source.articles.items && source.articles.items.slice) {
+		source.articles.items = source.articles.items.slice(0, 10);
 	}
 
-	res.render('uk', { layout: 'vanilla', articles: articles });
+	res.render('uk', { layout: 'vanilla', articles: source.articles, fastFt: source.fastFt });
