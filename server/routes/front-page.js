326cf238478b9082aeff22e070ccd66163ca506b
Imran Sulemanji <imranolas@gmail.com>
2015-06-16 12:03:30 +0100

    Minimize duplication

--- server/routes/front-page.js
+++ server/routes/front-page.js
@@ -2,46 +2,64 @@
 
 var ContentApi = require('../services/content-api');
 
-var uuids = {
-	ukTop: '520ddb76-e43d-11e4-9e89-00144feab7de', // list id
-	intlTop: 'b0d8e4fe-10ff-11e5-8413-00144feabdc0', // list id
-	fastFt: '5c7592a8-1f0c-11e4-b0cb-b2227cce2b54', // concept id
-}
+var pollConfig = {
+	ukTop: {
+		type: 'page',
+		uuid: '520ddb76-e43d-11e4-9e89-00144feab7de', // list id
+		useElasticSearch: true,
+		interval: 60 * 1000
+	},
+	intlTop: {
+		type: 'page',
+		uuid: 'b0d8e4fe-10ff-11e5-8413-00144feabdc0', // list id
+		useElasticSearch: true,
+		interval: 60 * 1000
+	},
+	fastFt: {
+		type: 'concept',
+		uuid: '5c7592a8-1f0c-11e4-b0cb-b2227cce2b54', // concept id
+		useElasticSearch: true,
+		interval: 30 * 1000
+	}
+};
 
 // Content cache for polling
 var content = {
 	elastic: {
-		articles: [],
+		ukTop: [],
+		intTop: [],
 		fastFt: []
 	},
 	capi1: {
-		articles: [],
+		ukTop: [],
+		intTop: [],
 		fastFt: []
 	}
-}
+};
 
 // Poll both APIs so that we can feature flag between them
 
-ContentApi.pollList(uuids.ukTop, true, function(page) {
-	content.elastic.articles = page;
-});
-
-ContentApi.pollList(uuids.ukTop, false, function(page) {
-	content.capi1.articles = page;
-});
-
-ContentApi.pollFastFt(uuids.fastFt, true, function(fastFt) {
-	content.elastic.fastFt = fastFt;
-});
-
-ContentApi.pollFastFt(uuids.fastFt, false, function(fastFt) {
-	content.capi1.fastFt = fastFt;
-});
-
-module.exports = function(req, res, next) {
-	var source = (res.locals.flags.elasticSearchItemGet.isSwitchedOn ? content.elastic : content.capi1);
-	if(source.articles.items && source.articles.items.slice) {
-		source.articles.items = source.articles.items.slice(0, 10);
+Object.keys(pollConfig).forEach(function(it) {
+	if(pollConfig[it].useElasticSearch) {
+		ContentApi.pollContent(pollConfig[it], true, function(page) {
+			content.elastic[it] = page;
+		});
 	}
 
-	res.render('uk', { layout: 'vanilla', articles: source.articles, fastFt: source.fastFt });
+	ContentApi.pollContent(pollConfig[it], false, function(page) {
+		content.capi1[it] = page;
+	});
+});
+
+module.exports = function(req, res) {
+	var source = (res.locals.flags.elasticSearchItemGet.isSwitchedOn ? content.elastic : content.capi1);
+	if(source.ukTop.items && source.ukTop.items.slice) {
+		source.ukTop.items = source.ukTop.items.slice(0, 10);
+	}
+
+	res.render('uk', {
+		layout: 'vanilla',
+		articles: source.ukTop,
+		fastFt: source.fastFt
+	});
+};
--- server/services/content-api.js
+++ server/services/content-api.js
@@ -1,61 +1,66 @@
 'use strict';
 
 var errorHandler = require('express-errors-handler');
-var Poller = require('ft-poller');
 var ApiClient = require('next-ft-api-client');
 
 var ftErrorHandler = (process.env.NODE_ENV === 'production') ? errorHandler.captureMessage : console.log;
 
 var uuid = function(thingUri) {
 	return thingUri.replace('http://api.ft.com/thing/', '');
-}
+};
 
-var fetchArticles = function(pageId, useElasticSearch) {
-	return ApiClient.lists({ uuid: pageId })
-	.then(function(list) {
-		var title = list.title;
-		var ids = list.items.map(function(it) { return uuid(it.id) });
+var fetchContent = {
+	page: function(pageId, useElasticSearch) {
+		return ApiClient.lists({ uuid: pageId })
+		.then(function(list) {
+			var title = list.title;
+			var ids = list.items.map(function(it) { return uuid(it.id); });
 
-		return ApiClient.contentLegacy({
-			uuid: ids,
-			useElasticSearch: useElasticSearch
-		}).then(function(articles) {
-			return {
-				title: title,
-				items: articles
-			}
-		})
-	});
-}
+			return ApiClient.contentLegacy({
+				uuid: ids,
+				useElasticSearch: useElasticSearch
+			}).then(function(articles) {
+				return {
+					title: title,
+					items: articles
+				};
+			});
+		});
+	},
 
-var fetchFastFt = function(conceptId, useElasticSearch) {
-	return ApiClient.contentAnnotatedBy({ uuid: conceptId, useElasticSearch: useElasticSearch })
-	.then(function(content) {
-		var ids = content;
+	concept: function(conceptId, useElasticSearch) {
+		return ApiClient.contentAnnotatedBy({ uuid: conceptId, useElasticSearch: useElasticSearch })
+		.then(function(content) {
+			var ids = content;
 
-		return ApiClient.content({
-			uuid: ids,
-			useElasticSearch: useElasticSearch
-		}).then(function(articles) {
-			return {
-				title: "fastFT",
-				items: articles
-			}
-		})
-	})
-}
+			return ApiClient.content({
+				uuid: ids,
+				useElasticSearch: useElasticSearch
+			}).then(function(articles) {
+				return {
+					title: "fastFT",
+					items: articles
+				};
+			});
+		});
+	}
+
+};
 
 var logFetched = function(list, useElasticSearch) {
 	var source = useElasticSearch ? 'elasticsearch' : 'CAPI';
 	console.log('Fetched list', list.title, 'with', list.items.length, 'articles from', source);
 
 	return list;
-}
+};
 
-var pollList = function(pageId, useElasticSearch, updateContent) {
-	var poller = function(data) {
-		fetchArticles(pageId, useElasticSearch)
-		.then(function(it) { return logFetched(it, useElasticSearch); })
+var pollContent = function(opt, useElasticSearch, updateContent) {
+	var poller = function(/*data*/) {
+
+		fetchContent[opt.type](opt.uuid, useElasticSearch)
+		.then(function(it) {
+			return logFetched(it, useElasticSearch);
+		})
 		.then(updateContent)
 		.catch(function(err) {
 			console.log("Error fetching from", (useElasticSearch ? "elasticsearch" : "CAPI"), err);
@@ -63,25 +68,12 @@ var pollList = function(pageId, useElasticSearch, updateContent) {
 	};
 
 	poller();
-	return setInterval(poller, 1*60*1000);
-}
-
-var pollFastFt = function(fastFtId, useElasticSearch, updateContent) {
-	var poller = function(data) {
-		fetchFastFt(fastFtId, useElasticSearch)
-		.then(function(it) { return logFetched(it, useElasticSearch); })
-
-		.then(updateContent)
-		.catch(function(err) {
-			console.log("Error fetching fastFT", err);
-		});
-	}
-
-	poller();
-	return setInterval(poller, 30*1000);
-}
+	return setInterval(
+		poller,
+		opt.interval
+	);
+};
 
 module.exports = {
-	pollList: pollList,
-	pollFastFt: pollFastFt
-}
+	pollContent: pollContent
+};
