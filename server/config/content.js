67f53afbb1bdc475ed595a6d930c96f6a99aa29d
Viktor Charypar <charypar@gmail.com>
2015-06-24 13:39:30 +0100

    Add topical lists and clean up a bit

---
+++ server/config/content.js
@@ -0,0 +1,69 @@
+'use strict';
+
+// UK
+
+const homeUk = {
+	top: {
+		type: 'page',
+		uuid: '520ddb76-e43d-11e4-9e89-00144feab7de',
+		elasticSearchSupported: true,
+		interval: 60 * 1000
+	},
+	fastFt: {
+		type: 'concept',
+		uuid: '5c7592a8-1f0c-11e4-b0cb-b2227cce2b54',
+		elasticSearchSupported: true,
+		interval: 30 * 1000
+	},
+	opinion: {
+		type: 'page',
+		uuid: 'bc81b5bc-1995-11e5-a130-2e7db721f996',
+		elasticSearchSupported: true,
+		interval: 60 * 1000
+	},
+	markets: {
+		type: 'page',
+		uuid: 'ce659fb4-199f-11e5-a130-2e7db721f996',
+		elasticSearchSupported: true,
+		interval: 60 * 1000
+	},
+	technology: {
+		type: 'page',
+		uuid: 'd990bc34-199f-11e5-a130-2e7db721f996',
+		elasticSearchSupported: true,
+		interval: 60 * 1000
+	},
+	lifestyle: {
+		type: 'page',
+		uuid: 'ba161cf0-199f-11e5-a130-2e7db721f996',
+		elasticSearchSupported: true,
+		interval: 60 * 1000
+	},
+	editors: {
+		type: 'page',
+		uuid: '73667f46-1a55-11e5-a130-2e7db721f996',
+		elasticSearchSupported: true,
+		interval: 60 * 1000
+	}
+};
+
+// International
+
+const homeIntl = {
+	top: {
+		type: 'page',
+		uuid: 'b0d8e4fe-10ff-11e5-8413-00144feabdc0',
+		elasticSearchSupported: true,
+		interval: 60 * 1000
+	},
+	fastFt: {
+		type: 'concept',
+		uuid: '5c7592a8-1f0c-11e4-b0cb-b2227cce2b54',
+		elasticSearchSupported: true,
+		interval: 30 * 1000
+	}
+};
+
+export default {
+	uk: homeUk
+};
--- server/routes/front-page.js
+++ server/routes/front-page.js
@@ -1,57 +1,34 @@
 'use strict';
+import {uk as contentConfig} from '../config/content'
 import {pollContent} from '../services/content-api';
 
-const pollConfig = {
-	ukTop: {
-		type: 'page',
-		uuid: '520ddb76-e43d-11e4-9e89-00144feab7de', // list id
-		useElasticSearch: true,
-		interval: 60 * 1000
-	},
-	intlTop: {
-		type: 'page',
-		uuid: 'b0d8e4fe-10ff-11e5-8413-00144feabdc0', // list id
-		useElasticSearch: true,
-		interval: 60 * 1000
-	},
-	fastFt: {
-		type: 'concept',
-		uuid: '5c7592a8-1f0c-11e4-b0cb-b2227cce2b54', // concept id
-		useElasticSearch: true,
-		interval: 30 * 1000
-	}
-};
-
 // Content cache for polling
+
 var content = {
 	elastic: {
-		ukTop: [],
-		intTop: [],
+		top: [],
 		fastFt: []
 	},
 	capi1: {
-		ukTop: [],
-		intTop: [],
+		top: [],
 		fastFt: []
 	}
 };
 
 // Poll both APIs so that we can feature flag between them
 
-// Poll both APIs so that we can feature flag between them
-
-Object.keys(pollConfig)
+Object.keys(contentConfig)
 .forEach(it => {
-	if(pollConfig[it].useElasticSearch) {
+	if(contentConfig[it].useElasticSearch) {
 		pollContent(
-			pollConfig[it],
+			contentConfig[it],
 			true,
 			page => content.elastic[it] = page
 		);
 	}
 
 	pollContent(
-		pollConfig[it],
+		contentConfig[it],
 		false,
 		page => content.capi1[it] = page
 	);
@@ -59,13 +36,13 @@ Object.keys(pollConfig)
 
 module.exports = function(req, res) {
 	var source = (res.locals.flags.elasticSearchItemGet.isSwitchedOn ? content.elastic : content.capi1);
-	if(source.ukTop.items && source.ukTop.items.slice) {
-		source.ukTop.items = source.ukTop.items.slice(0, 10);
+	if(source.top.items && source.top.items.slice) {
+		source.top.items = source.top.items.slice(0, 10);
 	}
 
 	res.render('uk', {
 		layout: 'wrapper',
-		articles: source.ukTop,
+		articles: source.top,
 		fastFt: source.fastFt
 	});
 };
