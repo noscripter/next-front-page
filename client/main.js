6201ad3fc4f75c76705f628e6e7d1002de5db962
Viktor Charypar <charypar@gmail.com>
2015-07-07 16:33:45 +0100

    Tab state persistence

--- client/main.js
+++ client/main.js
@@ -4,6 +4,7 @@ import oDate from 'o-date';
 import setup from 'next-js-setup';
 import fastFT from '../components/fastft/main';
 import headerFooter from 'n-header-footer';
+import headerTabs from './components/header-tabs/main';
 
 setup.bootstrap((result) => {
 	var flags = result.flags;
@@ -12,6 +13,10 @@ setup.bootstrap((result) => {
 
 	const feedContainer = document.getElementById("fastft");
 	fastFT.init(feedContainer);
+
+	const tabs = document.getElementById("header-tabs");
+	console.log(tabs);
+	headerTabs.init(tabs, "#news-tab");
 
 	oDate.init();
 });
