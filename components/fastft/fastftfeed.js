7c835c533b7eb050fe552529851b40c32249e311
Viktor Charypar <charypar@gmail.com>
2015-07-09 21:56:50 +0100

    Acceibility WIP

--- components/fastft/fastftfeed.js
+++ components/fastft/fastftfeed.js
@@ -6,11 +6,11 @@ import {Feed} from '../feed/main';
 class FastFtFeed extends React.Component {
 	render() {
 		return (
-			<div>
+			<div aria-live="assertive">
 				<h1>
 					<a href="/fastft" data-trackable="go-to-link">
 						<span className="fastft__logo__fast">fast</span>
-						<span className="fastft__logo__ft">FT</span>
+						<span className="fastft__logo__ft" aria-label="ef tee">FT</span>
 						<i className="fastft__more-icon more-icon--fastft" />
 					</a>
 				</h1>
--- components/feed/article.js
+++ components/feed/article.js
@@ -19,7 +19,7 @@ class Article extends React.Component {
 				<a href={linkHref(id)} tabIndex="0" data-trackable="feed-link">
 					<h2>{title}</h2>
 					<span>
-						<i className="time-icon" />
+						<i className="time-icon" aria-label="Published" />
 						<time data-o-component="o-date" className="o-date" dateTime={publishedDate}>
 							{format(publishedDate, dateFormat)}
 						</time>
