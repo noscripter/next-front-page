7e602429b247e30d0a1b6084844795477b91a1c9
Viktor Charypar <charypar@gmail.com>
2015-07-14 16:15:52 +0100

    Merge schema, types and entry points

--- server/graphql/schema.js
+++ server/graphql/schema.js
@@ -1,32 +1,114 @@
+import {Promise} from 'es6-promise';
+
 import {
   graphql,
   GraphQLSchema,
   GraphQLObjectType,
+  GraphQLNonNull
 } from 'graphql';
 
-import {
-	TopStories,
-	FastFT,
-	EditorsPicks,
-	Opinion,
-	Lifestyle,
-	Markets,
-	Technology,
-	Popular
-} from './entry-points';
+import {Region} from './types/basic'
+import {Collection} from './types/collections'
+
+import sources from './config/sources';
+import backend from './backend';
 
 const queryType = new GraphQLObjectType({
 	name: 'Query',
 	description: 'FT content API',
 	fields: () => ({
-		top: TopStories,
-		fastFT: FastFT,
-		editorsPicks: EditorsPicks,
-		opinion: Opinion,
-		lifestyle: Lifestyle,
-		markets: Markets,
-		technology: Technology,
-		popular: Popular
+		top: {
+			type: Collection,
+			args: {
+				region: { type: new GraphQLNonNull(Region) }
+			},
+			resolve: (root, {region}) => {
+				let uuid = sources[`${region}Top`].uuid;
+
+				return backend.page(uuid);
+			}
+		},
+		fastFT: {
+			type: Collection,
+			resolve: (root) => {
+				let uuid = sources.fastFt.uuid;
+
+				return backend.byConcept(uuid, 'fastFT');
+			}
+		},
+		editorsPicks: {
+			type: Collection,
+			resolve: (root) => {
+				// HACK this is waiting for editorial to start managing an Editor's picks list
+				let config = ['bigRead', 'lunch', 'management', 'frontPageSkyline', 'personInNews', 'lex'];
+
+				let promises = config
+				.map((it) => ({
+					type: sources[it].type,
+					uuid: sources[it].uuid
+				}))
+				.map((it) => {
+					switch(it.type) {
+						case 'page':
+							return backend.page(it.uuid)
+							.then(page => page.items[0]);
+						case 'search':
+							return backend.search(it.uuid)
+							.then(ids => ids[0]);
+						default:
+							throw "Unknown type: " + it.type;
+					}
+				})
+
+				return Promise.all(promises)
+				.then(ids => ({
+					title: "Editor's picks",
+					conceptId: null,
+					sectionId: null,
+					items: ids
+				}))
+			}
+		},
+		opinion: {
+			type: Collection,
+			resolve: (root) => {
+				let {uuid, sectionsId} = sources.opinion
+
+				return backend.page(uuid, sectionsId);
+			}
+		},
+		lifestyle: {
+			type: Collection,
+			resolve: (root) => {
+				let {uuid, sectionsId} = sources.lifestyle
+
+				return backend.page(uuid, sectionsId);
+			}
+		},
+		markets: {
+			type: Collection,
+			resolve: (root) => {
+				let {uuid, sectionsId} = sources.markets
+
+				return backend.page(uuid, sectionsId);
+			}
+		},
+		technology: {
+			type: Collection,
+			resolve: (root) => {
+				let {uuid, sectionsId} = sources.technology
+
+				return backend.page(uuid, sectionsId);
+			}
+		},
+		popular: {
+			type: Collection,
+			resolve: (root) => {
+				let url = sources.popular.uuid;
+
+				return backend.popular(url, 'Popular');
+			}
+		}
 	})
 });
 
--- server/graphql/types.js
+++
@@ -1,7 +0,0 @@
-import {Region} from './types/basic'
-import {Collection} from './types/collections'
-
-export default {
-	Region,
-	Collection
-}
