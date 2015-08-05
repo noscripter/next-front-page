4d6d3978c657e8aa71674eaaf2f5f68c68d9a2e7
Imran Sulemanji <imranolas@gmail.com>
2015-08-05 14:57:27 +0100

    Render videos and styles for 1440

--- client/main.js
+++ client/main.js
@@ -4,6 +4,8 @@ import headerTabs from './components/header-tabs/main';
 import setup from 'next-js-setup';
 import headerFooter from 'n-header-footer';
 import oDate from 'o-date';
+import nVideo from 'n-video';
+
 setup.bootstrap(({flags}) => {
 	headerFooter.init(flags);
 
@@ -13,6 +15,12 @@ setup.bootstrap(({flags}) => {
 	const tabs = document.getElementById("header-tabs");
 	headerTabs.init(tabs, "#news-tab");
 
+	nVideo.init({
+    optimumWidth: 710,
+    placeholder: true,
+    classes: ['video'],
+    selector: '.js-video'
+	});
 	oDate.init();
 });
 
