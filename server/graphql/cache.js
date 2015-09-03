54a5629d197a395e12253a46f9d8434a4bd2a7b7
Viktor Charypar <charypar@gmail.com>
2015-09-03 18:39:42 +0100

    Deduplicate backend request through the cache layer

--- server/graphql/cache.js
+++ server/graphql/cache.js
@@ -4,6 +4,7 @@ class Cache {
 	constructor(staleTtl) {
 		// in-memory content cache
 		this.contentCache = {};
+		this.requestMap = {};
 
 		const sweeper = () => {
 			const now = (new Date().getTime()) / 1000;
@@ -19,6 +20,10 @@ class Cache {
 		setInterval(sweeper, 60 * 1000);
 	}
 
+	clear(key) {
+		delete this.contentCache[key];
+	}
+
 	// Caching wrapper. Always returns a promise, when cache expires
 	// returns stale data immediately and fetches fresh one
 	cached(key, ttl, fetcher) {
@@ -31,8 +36,18 @@ class Cache {
 		// we have fresh data
 		if(expire > now && data) { return Promise.resolve(data); }
 
-		// fetch fresh data
-		const eventualData = fetcher()
+		// we don't have fresh data, fetch it
+		const eventualData = this._fetch(key, now, ttl, fetcher);
+
+		// return stale data or promise of fresh data
+		return (data ? Promise.resolve(data) : eventualData);
+	}
+
+	_fetch(key, now, ttl, fetcher) {
+		if(this.requestMap[key])
+			return this.requestMap[key];
+
+		this.requestMap[key] = fetcher()
 		.then((it) => {
 			let expireTime = now + ttl;
 
@@ -41,11 +56,15 @@ class Cache {
 				data: it
 			};
 
+			delete this.requestMap[key];
+
 			return it;
+		})
+		.catch((e) => {
+			delete this.requestMap[key];
 		});
 
-		// return stale data or promise of fresh data
-		return (data ? Promise.resolve(data) : eventualData);
+		return this.requestMap[key];
 	}
 }
 
